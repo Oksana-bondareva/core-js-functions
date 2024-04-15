76116d8ca090918e4a2c774a3d9d22b8918d28fa
Oksana Bondareva <oksana20-89@mail.ru>
2024-04-16 02:14:57 +0300

    feat: add solution task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -176,8 +176,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const stringArgs = args.map((v) => JSON.stringify(v)).join(',');
+    logFunc(`${func.name}(${stringArgs}) starts`);
+    const res = func(...args);
+    logFunc(`${func.name}(${stringArgs}) ends`);
+    return res;
+  };
 }
 
 /**
